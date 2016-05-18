07fd0eecf93779b24c07d3f3fdf0c9a88f6012b1
Jeremy Perez <perezje43@gmail.com>
2016-05-18 14:22:12 -0700

    Finished MVP

--- submissions/jeremy-shawn/scripts/article.js
+++ submissions/jeremy-shawn/scripts/article.js
@@ -20,10 +20,10 @@
     return template(this);
   };
 
-  // TODO: Set up a DB table for articles.
+  // DONE: Set up a DB table for articles.
   Article.createTable = function(callback) {
     webDB.execute(
-      '...;', // what SQL command do we run here inside these quotes?
+      'CREATE TABLE IF NOT EXISTS articleData (id INTEGER PRIMARY KEY, title VARCHAR, category VARCHAR, author VARCHAR, author_url VARCHAR, published_on VARCHAR, body VARCHAR);', // what SQL command do we run here inside these quotes?
       function(result) {
         console.log('Successfully set up the articles table.', result);
         if (callback) callback();
@@ -46,13 +46,14 @@
         (most recent article first!), and then hand off control to the View.
 
       If the DB is empty, we need to retrieve the JSON and process it. */
-    webDB.execute('...', function(rows) { // TODO: fill these quotes to 'select' our table.
+    webDB.execute('SELECT * FROM  articleData', function(rows) { // TODO: fill these quotes to 'select' our table.
       if (rows.length) {
         /* TODO:
            1 - Use Article.loadAll to instanitate these rows,
            2 - Pass control to the view by calling the next function that
                 was passed in to Article.fetchAll */
-
+        Article.loadAll(rows);
+        next();
       } else {
         $.getJSON('/data/hackerIpsum.json', function(data) {
           // Save each article from this JSON file, so we don't need to request it next time:
@@ -61,14 +62,15 @@
             /* TODO:
                1 - 'insert' the newly-instantiated article in the DB:
                 (hint: what can we call on each 'article' instance?). */
-
+            article.insertRecord();
           });
           // Now get ALL the records out the DB, with their database IDs:
-          webDB.execute('', function(rows) { // TODO: select our now full table
+          webDB.execute('SELECT * FROM articleData', function(rows) { // TODO: select our now full table
             // TODO:
             // 1 - Use Article.loadAll to instanitate these rows,
             // 2 - Pass control to the view by calling the next function that was passed in to Article.fetchAll
-
+            Article.loadAll(rows);
+            next();
           });
         });
       }
@@ -81,9 +83,9 @@
         {
           // TODO: Insert an article instance into the database:
           // Note: this method will be called elsewhere after we retrieve our JSON
-          'sql': '...;',
+          'sql': 'INSERT INTO articleData (title, author, author_url, category, published_on, body) VALUES ( ?, ?, ?, ?, ?, ?);',
           'data': [this.title, this.author, this.authorUrl, this.category, this.publishedOn, this.body]
-        } 
+        }
       ],
       callback
     );
@@ -97,7 +99,7 @@
               its properties into the corresponding record in the database: */
           /* Note: this is an advanced admin option, so you will need to test
               out an individual query in the SQL console */
-          'sql': '...;',
+          'sql': 'UPDATE articleData SET title=?, author=?, author_url=?, category=?, publish_on=?, body=? WHERE id=?;',
           'data': [this.title, this.author, this.authorUrl, this.category, this.publishedOn, this.body, this.id]
         }
       ],
@@ -112,7 +114,7 @@
           // TODO: Delete an article instance from the database based on its id:
           /* Note: this is an advanced admin option, so you will need to test
               out an individual query in the SQL console */
-          'sql': '...;',
+          'sql': 'DELETE FROM articleData WHERE id=?;',
           'data': [this.id]
         }
       ],
@@ -123,7 +125,7 @@
   Article.truncateTable = function(callback) {
     webDB.execute(
       // TODO: Use correct SQL syntax to delete all records from the articles table.
-      'DELETE ...;', // <----finish the command here, inside the quotes.
+      'DELETE articleData;', // <----finish the command here, inside the quotes.
       callback
     );
   };
